c7572e1effddacc87064e8bc1eef2f5f094896a3
Artem V. Navrotskiy <bozaro@yandex.ru>
2021-08-18 12:09:06 +0300

    Fix registry waiting

--- registry.sh
+++ registry.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 
-function wait_port {
-  echo Wait for service on port: $1
+function wait_url {
+  echo Wait for service on endpoint: $1
   n=0
-  until nc -z localhost $1; do
+  until curl -sf $1; do
     sleep 0.1
     n=$((n + 1))
     if [ "$n" -gt 300 ]; then
-      echo Waiting port $1 timeout
+      echo Waiting url $1 timeout
       exit 1
     fi
   done
@@ -18,11 +18,11 @@ function wait_port {
 # Start docker registry
 if [ "$(docker ps -q -f name=$REGISTRY_NAME)" == "" ]; then
   docker run --rm -d -p $REGISTRY_PORT:5000 --name $REGISTRY_NAME registry:2
-  wait_port $REGISTRY_PORT
+  wait_url http://localhost:$REGISTRY_PORT
 fi
 
 # Start docker proxy
 if [ "$(docker ps -q -f name=$PROXY_NAME)" == "" ]; then
   docker run --rm -d -e REGISTRY_PROXY_REMOTEURL=https://registry-1.docker.io -p $PROXY_PORT:5000 --name $PROXY_NAME registry:2
-  wait_port $PROXY_PORT
+  wait_url http://localhost:$PROXY_PORT
 fi
